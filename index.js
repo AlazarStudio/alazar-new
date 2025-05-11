9e78912d41282003531b163e6aeff292dfb16f84
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2025-05-11 21:42:27 +0300

    upd

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const https = require('https');
 const express = require('express');
 const path = require('path');
+const developersRouter = require('./routes/developers');
+const categoriesRouter = require('./routes/categories');
+const casesRouter = require('./routes/cases');
 
 const app = express();
 
@@ -14,9 +17,9 @@ const sslOptions = {
 // Загружаем middleware и маршруты
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'server/uploads')));
-app.use('/api/developers', await import('./routes/developers.js').then(m => m.default));
-app.use('/api/categories', await import('./routes/categories.js').then(m => m.default));
-app.use('/api/cases', await import('./routes/cases.js').then(m => m.default));
+app.use('/api/developers', developersRouter);
+app.use('/api/categories', categoriesRouter);
+app.use('/api/cases', casesRouter);
 
 // Запуск HTTPS-сервера
 https.createServer(sslOptions, app).listen(443, () => {
